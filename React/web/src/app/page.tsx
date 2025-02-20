a0e25ac301307f673043e746c27dd17416805ae5
rafaellindemann <rafaelld@gmail.com>
2025-02-20 02:21:24 -0300

    add fontes

--- React/web/src/app/page.tsx
+++ React/web/src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main>
       <div className="text-blue-400">Hello world!</div>
+
+      <h1 className="text-4xl font-semibold font-heading">lkdjfnls</h1>
       <Button text={"Rafael"}/>
       <Button text="NLW"/>
       <Button />
